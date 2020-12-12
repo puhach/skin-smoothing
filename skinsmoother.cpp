ea6019c8842505ecc5d228960e137828a5ca74a9
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2020-12-12 22:13:54 +0200

    Find the dominant color from the histogram

--- skinsmoother.cpp
+++ skinsmoother.cpp
@@ -1,27 +1,41 @@
 #include "skinsmoother.h"
 
 #include <opencv2/imgproc.hpp>
+
+#include <opencv2/highgui.hpp>	// TEST!
 #include <iostream>	// TEST!
 
 void SkinSmoother::applyInPlace(cv::Mat& image)	// virtual
 {
-	// If you use cvtColor with 8-bit images, the conversion will have some information lost. For many applications, this will not be noticeable 
-	// but it is recommended to use 32-bit images in applications that need the full range of colors or that convert an image before an operation 
-	// and then convert back.
-	// https://docs.opencv.org/4.2.0/d8/d01/group__imgproc__color__conversions.html#ga397ae87e1288a81d2363b61574eb8cab
-	cv::Mat imageHSVF;
-	image.convertTo(imageHSVF, CV_32F, 1.0 / 255);
-	cv::cvtColor(imageHSVF, imageHSVF, cv::COLOR_BGR2HSV);
+	//// If you use cvtColor with 8-bit images, the conversion will have some information lost. For many applications, this will not be noticeable 
+	//// but it is recommended to use 32-bit images in applications that need the full range of colors or that convert an image before an operation 
+	//// and then convert back.
+	//// https://docs.opencv.org/4.2.0/d8/d01/group__imgproc__color__conversions.html#ga397ae87e1288a81d2363b61574eb8cab
+	//cv::Mat imageHSVF;
+	//image.convertTo(imageHSVF, CV_32F, 1.0 / 255);
+	//cv::cvtColor(imageHSVF, imageHSVF, cv::COLOR_BGR2HSV);
 	//std::cout << imageHSVF << std::endl;
-	/*cv::Mat imageHSV;
-	cv::cvtColor(image, imageHSV, cv::COLOR_BGR2HSV);*/
+	cv::Mat imageHSV;
+	cv::cvtColor(image, imageHSV, cv::COLOR_BGR2HSV);
 
 	cv::Mat hist;
 	int channels[] = { 0, 1 };
-	int histSize[] = { 360, 256 }; //{ 180, 256 };
-	const float hueRange[] = { 0, 360 }, satRange[] = {0, 1}, *ranges[] = { hueRange, satRange };
-	cv::calcHist(&imageHSVF, 1, channels, cv::Mat(), hist, 2, histSize, ranges, true, false);
+	//int histSize[] = { 360, 256 }; 
+	int histSize[] = { 180, 256 };
+	//const float hueRange[] = { 0, 360 }, satRange[] = {0, 1}, *ranges[] = { hueRange, satRange };
+	const float hueRange[] = { 0, 180 }, satRange[] = { 0, 256 }, * ranges[] = { hueRange, satRange };
+	//cv::calcHist(&imageHSVF, 1, channels, cv::Mat(), hist, 2, histSize, ranges, true, false);
+	cv::calcHist(&imageHSV, 1, channels, cv::Mat(), hist, 2, histSize, ranges, true, false);
 	
+
+	// Find the dominant hue-saturation pair
+	cv::Point maxLoc;
+	double maxVal;
+	//cv::minMaxLoc(hist, nullptr, &maxVal, nullptr, &maxLoc);
+	int dominantColor[2];
+	cv::minMaxIdx(hist, nullptr, &maxVal, nullptr, dominantColor);
+
+
 
 	// TODO: implement skin smoothing
 
