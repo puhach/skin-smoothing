235d40a13938f6b81434b67a02523ace3bd82d38
Yaroslav Pugach <pugach.yaroslav2@gmail.com>
2020-12-12 21:54:15 +0200

    Calculate the histogram for hue and saturation channels

--- skinsmoother.cpp
+++ skinsmoother.cpp
@@ -12,7 +12,16 @@ void SkinSmoother::applyInPlace(cv::Mat& image)	// virtual
 	cv::Mat imageHSVF;
 	image.convertTo(imageHSVF, CV_32F, 1.0 / 255);
 	cv::cvtColor(imageHSVF, imageHSVF, cv::COLOR_BGR2HSV);
-	std::cout << imageHSVF << std::endl;
+	//std::cout << imageHSVF << std::endl;
+	/*cv::Mat imageHSV;
+	cv::cvtColor(image, imageHSV, cv::COLOR_BGR2HSV);*/
+
+	cv::Mat hist;
+	int channels[] = { 0, 1 };
+	int histSize[] = { 360, 256 }; //{ 180, 256 };
+	const float hueRange[] = { 0, 360 }, satRange[] = {0, 1}, *ranges[] = { hueRange, satRange };
+	cv::calcHist(&imageHSVF, 1, channels, cv::Mat(), hist, 2, histSize, ranges, true, false);
+	
 
 	// TODO: implement skin smoothing
 
